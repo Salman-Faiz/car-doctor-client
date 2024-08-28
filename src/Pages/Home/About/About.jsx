90e3f99755b7ce7341cb5869244585f992d62142
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-08-28 12:46:48 +0600

    About us section added

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -1,19 +1,33 @@
+import person from "../../../assets/images/about_us/person.jpg";
+import parts from "../../../assets/images/about_us/parts.jpg";
 const About = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero  min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="lg:w-1/2 relative">
+          <img
+            src={person}
+            className="w-3/4 rounded-lg shadow-2xl h-[475px] "
+          />
+          <img
+            src={parts}
+            className="w-1/2 rounded-lg shadow-2xl absolute right-5 top-1/2 h-[330px] border-8 border-white"
+          />
+        </div>
+        <div className="lg:w-1/2 space-y-4 p-4">
+          <p className="text-orange-500 text-4xl font-bold">About Us</p>
+          <h1 className="text-5xl font-bold pb-10">
+            We are qualified & of experience in this field
+          </h1>
+          <p className="py-6 ">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. the majority have suffered alteration in some form, by
+            injected humour, or randomised words which don't look even slightly
+            believable.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn bg-orange-500">Get more info</button>
         </div>
       </div>
     </div>
