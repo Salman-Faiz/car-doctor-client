32c377f8a0044cb42070bc94a77c64305f101593
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-09-02 14:27:44 +0600

    create checkout form and show data using context

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,12 +1,98 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const CheckOut = () => {
   const services = useLoaderData();
-  const { title, price } = services;
+  const { title, price, _id } = services;
+  const { user } = useContext(AuthContext);
+
+  const handleCheckOut = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = user?.email;
+    const date = form.date.value;
+    const price = form.price.value;
+
+    const order = {
+      CustomerName: name,
+      Email: email,
+      PurchaseDate: date,
+      price: price,
+      id: _id,
+    };
+    console.log(order);
+  };
   return (
     <div>
       <h2>CheckOut Services: {title}</h2>
       <p>price: {price}</p>
+
+      <form onSubmit={handleCheckOut} className="card-body ">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Customer Name</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              defaultValue={user?.displayName}
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              className="input input-bordered"
+              required
+              defaultValue={user?.email}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Order Date</span>
+            </label>
+            <input
+              type="date"
+              name="date"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Price</span>
+            </label>
+            <input
+              type="text"
+              placeholder="price"
+              name="price"
+              className="input input-bordered"
+              required
+              defaultValue={price}
+            />
+          </div>
+          <div className="col-span-2 lg:h-32 border-2 rounded-xl p-3">
+            <textarea name="message" id="" placeholder="message"></textarea>
+          </div>
+        </div>
+        <div className="form-control mt-6">
+          <input
+            className="btn btn-error btn-block font-bold text-xl"
+            type="submit"
+            value="Order Confirm"
+          />
+        </div>
+      </form>
     </div>
   );
 };
